336b97106c709a313af9c79640a30f480143a9fb
Wilson Henao <wilson.henao@utp.edu.co>
2023-05-22 13:19:24 -0500

    Cambio en ruta

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" Component={Home} />
+        <Route path="/home" Component={Home} />
 
       </Routes>
     </BrowserRouter>
