f155e569a514f6ba5171041f459ef7fa487cc61c
Wilson Henao <wilson.henao@utp.edu.co>
2023-05-21 22:52:39 -0500

    Mejorar estilo del navbar

--- src/components/Navbar1.jsx
+++ src/components/Navbar1.jsx
@@ -4,17 +4,14 @@ import "../styles/Navbar.css";
 export function Navbar1() {
   return (
     <div className="background-navbar">
-      <div className="navbar1">
-        <Link id ="text1" className="navbar1" to="/">
-          {" "}
-          Sistema De Gestion De recursos Humanos
-        </Link>
-      </div>
-      <div className="navbar2">
-        <Link id ="text2" className="navbar2" to="/">
-          {" "}
-          Home
-        </Link>
+      <div className="container">
+        <div className="navbar1">
+          <img src="/logo.png" alt="" className="logo"/>
+          <Link to="/" className="navbar">Sistema de Gestión De Recursos Humanos</Link>
+        </div>
+        <div className="navbar2">
+          <Link to="/" className="navbar">Home</Link>
+        </div>
       </div>
     </div>
   );
