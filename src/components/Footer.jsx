937126767ae4f512c2368fb8de3604bdb85561fd
Wilson Henao <wilson.henao@utp.edu.co>
2023-05-22 16:59:04 -0500

    Se agrega componente footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,22 @@
+import "../styles/Footer.css";
+
+
+
+export function Footer() {
+  return (
+    <>
+      <div className="footer">
+        <div>
+          
+        </div>
+        <div className="texto1">
+          <p>Pereira - Colombia</p>
+        </div>
+        <div className="texto2">
+          
+          <p>Español</p>
+        </div>
+      </div>
+    </>
+  );
+}
