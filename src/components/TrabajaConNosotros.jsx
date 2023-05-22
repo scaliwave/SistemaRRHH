65e4c0f6b1c3ee99b0bdf514fb48a0750076253f
Wilson Henao <wilson.henao@utp.edu.co>
2023-05-21 22:59:54 -0500

    Organización de trabaja con nosotros

--- src/components/TrabajaConNosotros.jsx
+++ src/components/TrabajaConNosotros.jsx
@@ -2,13 +2,15 @@ export function TrabajaConNosotros() {
   return (
     <>
       <div className="trabajaAqui">
-        <h1>Trabaja con nosotros</h1>
-        <hr className="hr" />
-        <p>
-          {" "}
-          Entérate de las numerosas ofertas y postulate a la que mas te llame la
-          atención.
-        </p>
+        <div>
+          <h1>Trabaja con nosotros</h1>
+          <hr className="hr" />
+          <p>
+            {" "}
+            Entérate de las numerosas ofertas y postulate a la que mas te llame la
+            atención.
+          </p>
+        </div>
       </div>
     </>
   );
