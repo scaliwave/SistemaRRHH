6fe5551066ae76ee92c529517ec47c8d3dd1c3a4
Thomas Olaya <thomasog10@gmail.com>
2023-05-22 13:09:17 -0500

    organice el chatbot

--- src/components/ChatBot.jsx
+++ src/components/ChatBot.jsx
@@ -1,3 +1,16 @@
+import chatbot from '/public/chatbot.png'
+import "../styles/Chatbot.css"
+
+
 export function ChatBot() {
-  return <div>Pregúntanos</div>;
+  return (
+    <div className='chatbot'>
+      <div className='imagen'>
+        <img src={chatbot} width="70px" height="70px"/>
+      </div>
+      <div>
+        <h3 className='mensaje'>Contáctanos</h3>
+      </div>
+    </div>
+  );
 }
