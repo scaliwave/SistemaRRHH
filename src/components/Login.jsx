ff673b23102ec23c8694937dab3b290900287e9a
Wilson Henao <wilson.henao@utp.edu.co>
2023-05-22 00:02:32 -0500

    Se organiza login, y se modifica ruta de home

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,39 +4,9 @@ export function Login() {
   return (
     <>
       <div className="login">
-        <form>
-          <div className="mb-3">
-            <label className="form-label">Email address</label>
-            <input
-              type="email"
-              className="form-control"
-              id="exampleInputEmail1"
-              aria-describedby="emailHelp"
-            />
-            <div id="emailHelp" className="form-text">
-              Well never share your email with anyone else.
-            </div>
-          </div>
-          <div className="mb-3">
-            <label className="form-label">Password</label>
-            <input
-              type="password"
-              className="form-control"
-              id="exampleInputPassword1"
-            />
-          </div>
-          <div className="mb-3 form-check">
-            <input
-              type="checkbox"
-              className="form-check-input"
-              id="exampleCheck1"
-            />
-            <label className="form-check-label">Check me out</label>
-          </div>
-          <button type="submit" className="btn btn-primary">
-            Submit
-          </button>
-        </form>
+        <div className="texto">
+          INGRESO EMPLEADO
+        </div>
       </div>
     </>
   );
