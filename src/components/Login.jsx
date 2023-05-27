6e8a1bd49dec1712641aafb41713d78ff13c15d9
Wilson Henao <wilson.henao@utp.edu.co>
2023-05-27 12:29:12 -0500

    Se agrega funcionalidad de logueo

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,28 +1,50 @@
+import { useState } from "react";
 import "../styles/login.css";
+import axios from "axios";
 
 export function Login() {
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const usuarioAccess = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post('http://localhost:3000/api/login', {username, password});
+
+      localStorage.setItem('token', response.data.token)
+
+      window.location.href = '/administrador'
+    } catch (error) {
+      console.error("Error al iniciar sesion:", error)
+    }
+  };
+
   return (
     <>
       <div className="login">
         <div className="titleLogin">
           <h1>INGRESO EMPLEADO</h1>
         </div>
-        <form className="formLogin">
+        <form className="formLogin" onSubmit={usuarioAccess}>
           <div className="userLogin">
-            <label className="form-label">usuario</label>
+            <label className="form-label">Usuario</label>
             <input
               type="email"
               className="form-control"
               id="exampleInputEmail1"
               placeholder="Enter your email address"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
             />
             <div id="emailHelp" className="Email"></div>
           </div>
           <div className="passLogin">
             <label className="form-label">Contraseña</label>
-            <input type="password" className="form-control" placeholder="Enter your password" />
+            <input type="password" className="form-control" placeholder="Enter your password" value={password} onChange={(e) => setPassword(e.target.value)}/>
           </div>
-          <button type="submit" className="botonLogin" onClick={(e)=>{console.log(e.target)}}>
+          <button type="submit" className="botonLogin">
             Log In
           </button>
           <p>Olvidaste tu contraseña?</p>
--- src/pages/Administrador.jsx
+++ src/pages/Administrador.jsx
@@ -1,32 +1,53 @@
 import "../styles/backgroundMain.css";
 import "../styles/admin.css";
+import axios from "axios";
 
 import { BloqueTitulo } from "../components/BloqueTitulo";
 import { Navbar2 } from "../components/Navbar2";
 import { VerticalBar } from "../components/VerticalBar";
 import { UserBlock } from "../components/UserBlock";
 import { UserMenu } from "../components/UserMenu";
+import { useEffect } from "react";
 
 export function Administrador() {
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/api/usuarios', {
+      headers: {
+        authorization: `Bearer ${localStorage.getItem('token')}`
+      }
+    })
+      .then(response => {
+        console.log(response)
+      })
+      .catch(error => {
+        console.error('Error al obtener la página', error);
+      });
+  }, []);
+
   return (
-    <div className="background">
-      <Navbar2 />
-      <div className="adminContainer">
-        <div className="adminC1">
-          <VerticalBar />
-        </div>
-        <div className="adminC2">
-          <BloqueTitulo />
-          <div className="adminC2_1">
-            <div>
-              <UserBlock name={"Ligia López"} rol={"Administrador"} />
-            </div>
-            <div>
-              <UserMenu />
+    <div>
+      
+      <div className="background">
+        <Navbar2 />
+        <div className="adminContainer">
+          <div className="adminC1">
+            <VerticalBar />
+          </div>
+          <div className="adminC2">
+            <BloqueTitulo />
+            <div className="adminC2_1">
+              <div>
+                <UserBlock name={"Ligia López"} rol={"Administrador"} />
+              </div>
+              <div>
+                <UserMenu />
+              </div>
             </div>
           </div>
         </div>
       </div>
+      
     </div>
   );
 }
