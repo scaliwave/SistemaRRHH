0d1e85ed7fa58a5121188f793e304acc92f71990
Wilson Henao <wilson.henao@utp.edu.co>
2023-05-27 12:29:48 -0500

    Se agrega funcionalidad de deslogueo

--- src/components/Navbar2.jsx
+++ src/components/Navbar2.jsx
@@ -3,6 +3,11 @@ import { Link } from "react-router-dom";
 import "../styles/navbar2.css";
 
 export function Navbar2() {
+
+  const logout = (e) => {
+    localStorage.removeItem('token')
+  }
+  
   return (
     <div className="navbarGroup">
       <div className="container">
@@ -10,7 +15,7 @@ export function Navbar2() {
           Administrador
         </Link>
         <p className="userNavbar">hola</p>
-        <Link to="/">
+        <Link to="/" onClick={logout()}>
           <img src="/exit.png" className="exit" />
         </Link>
       </div>
