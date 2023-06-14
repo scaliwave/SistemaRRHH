97dff80880900e17a59020fc8dde55cb2552b8ef
Wilson Henao <wilson.henao@utp.edu.co>
2023-06-14 01:14:53 -0500

    Se agrega funcionalidad editar vacante

--- src/components/GestionVacantes.jsx
+++ src/components/GestionVacantes.jsx
@@ -3,11 +3,72 @@ import axios from "axios";
 
 import "../styles/gestionarVacantes.css";
 
-import { BiEditAlt } from "react-icons/bi";
+import { BiEditAlt, BiSave } from "react-icons/bi";
+import { Btn1 } from "./Btn1";
 
 export function GestionVacantes() {
   const [vacantes, setVacantes] = useState([]);
+  const [width, setWidth] = useState(120);
+  const [editableNombre, setEditableNombre] = useState(false);
+  const [editableDescripcion, setEditableDescripcion] = useState(false)
   const [editingIndex, setEditingIndex] = useState(-1);
+
+  const editarVacante = (e, id) => {
+    e.preventDefault();
+    setVacantes((prevVacantes) => {
+      return prevVacantes.map((vacante) => {
+        if (vacante.id === id) {
+          return {
+            ...vacante,
+            editMode: !vacante.editMode,
+          };
+        }
+        return vacante;
+      });
+    });
+  };
+
+  const editarNombre = (index) => {
+    setEditableNombre(true)
+    setEditingIndex(index)
+  }
+
+  const editarDescripcion = (index) => {
+    setEditableDescripcion(true)
+    setEditingIndex(index)
+  }
+
+  const guardarNombre = () => {
+    setEditableNombre(false)
+  };
+
+  const guardarDescripcion = () => {
+    setEditableDescripcion(false)
+  }
+
+  useEffect(() => {
+    const updateVacante = async(id, vacante) => {
+      try {
+        const response = await axios.put(
+          `http://localhost:3000/api/vacantes/${id}`,
+          vacante,
+          {
+            headers: {
+              authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+          }
+        );
+      } catch (error) {
+        console.log(error)
+      }
+    };
+
+    vacantes.forEach((vacante) => {
+      if (vacante.editMode) {
+        updateVacante(vacante.id, vacante);
+      }
+    });
+  }, [vacantes]);
 
   useEffect(() => {
     const getVacantes = async () => {
@@ -34,10 +95,6 @@ export function GestionVacantes() {
     setVacantes(updatedVacantes);
   };
 
-  const handleEditClick = (index) => {
-    setEditingIndex(index);
-  };
-
   const handleImageChange = (event, index) => {
     const file = event.target.files[0];
     const reader = new FileReader();
@@ -62,41 +119,57 @@ export function GestionVacantes() {
           <div className="scroll">
             <div id="container">
               {vacantes.map((vacante, index) => (
-                <div className="vacante-container-" key={vacante.id}>
-                  <div id="content-vacante-">
-                    <div id="editCampo">
-                      {editingIndex === index ? (
+                <form onSubmit={(e) => editarVacante(e, vacante.id)}>
+                  <div className="vacante-container-" key={vacante.id}>
+                    <div id="content-vacante-">
+                      <div id="editCampo">
+                        {editableNombre && editingIndex === index ? (
+                          <div>
+                            <input
+                              type="text"
+                              value={vacante.nombre}
+                              onChange={(e) => handleNombreChange(e, index)}
+                            />
+                            <div className="editar"><BiSave onClick={guardarNombre}></BiSave></div>
+                          </div>
+                        ) : (
+                          <h3>{vacante.nombre}</h3>
+                        )}
+                        {vacante.editMode && !editableNombre && (<div className="editar"><BiEditAlt onClick={()=> {editarNombre(index)}} /></div>)}
+                      </div>
+                      <div id="editCampo">
+                        {editableDescripcion && editingIndex === index ? (
+                          <div>
+                            <textarea
+                              value={vacante.descripcion}
+                              onChange={(e) => handleDescripcionChange(e, index)}
+                            />
+                            <div className="editar"><BiSave onClick={guardarDescripcion}></BiSave></div>
+                          </div>
+                        ) : (
+                          <p>{vacante.descripcion}</p>
+                        )}
+                        {vacante.editMode && !editableDescripcion && (<div className="editar"><BiEditAlt onClick={() => editarDescripcion(index)} /></div>)}
+                      </div>
+                    </div>
+                    <div id="editImg-vacante">
+                      {/* <img src={vacante.imageUrl} alt="Vacante" /> */}
+                      <img src="/abstract-shape.png" alt="vacante" />
+                      {vacante.editMode && (
                         <input
-                          type="text"
-                          value={vacante.nombre}
-                          onChange={(e) => handleNombreChange(e, index)}
+                          type="file"
+                          onChange={(e) => handleImageChange(e, index)}
                         />
-                      ) : (
-                        <h3>{vacante.nombre}</h3>
                       )}
-                      <BiEditAlt onClick={() => handleEditClick(index)} />
                     </div>
-                    <div id="editCampo">
-                      {editingIndex === index ? (
-                        <textarea
-                          value={vacante.descripcion}
-                          onChange={(e) => handleDescripcionChange(e, index)}
-                        />
-                      ) : (
-                        <p>{vacante.descripcion}</p>
-                      )}
-                      <BiEditAlt onClick={() => handleEditClick(index)} />
+                    <div>
+                      <Btn1
+                        name={vacante.editMode ? "Guardar" : "Editar vacante"}
+                        size={[width, 30]}
+                      />
                     </div>
                   </div>
-                  <div id="editImg-vacante">
-                    {/* <img src={vacante.imageUrl} alt="Vacante" /> */}
-                    <img src="/abstract-shape.png" alt="vacante" />
-                    <input
-                      type="file"
-                      onChange={(e) => handleImageChange(e, index)}
-                    />
-                  </div>
-                </div>
+                </form>
               ))}
             </div>
           </div>
@@ -108,9 +181,6 @@ export function GestionVacantes() {
       <div className="menu-gestion-vacantes">
         <div>
           <button className="btn-gestion-vacantes">Previsualizar</button>
-        </div>
-        <div>
-          <button className="btn-gestion-vacantes">Guardar</button>
         </div>
       </div>
     </div>
